f01f1b7e47b41d1822f833db66a8bb86cb6c922e
HaiQuangDang <danghaiquang12@gmail.com>
2023-04-03 11:58:09 +0700

    message 3 - from IntelliJ IDEA

--- src/HelloJavaWorld.java
+++ src/HelloJavaWorld.java
@@ -4,5 +4,6 @@ public class HelloJavaWorld {
     public static void main(String[] args) {
         System.out.println("Hello Java World! I come from Eclipse IDE. Where do you come from?");
         System.out.println("Hey? Oh Thank God! There's still someone alive! I'm from NetBeans. Is anyone outhere?");
+        System.out.println("Oh hey! I'm here, I'm in IntelliJ IDEA, Are we the last?");
     }
 }
