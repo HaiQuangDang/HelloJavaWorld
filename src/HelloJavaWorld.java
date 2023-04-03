9d0acaeb9bde005786b68f709784b5bedfd99611
HaiQuangDang <danghaiquang12@gmail.com>
2023-04-03 12:05:46 +0700

    the really last message

--- src/HelloJavaWorld.java
+++ src/HelloJavaWorld.java
@@ -5,5 +5,6 @@ public class HelloJavaWorld {
         System.out.println("Hello Java World! I come from Eclipse IDE. Where do you come from?");
         System.out.println("Hey? Oh Thank God! There's still someone alive! I'm from NetBeans. Is anyone outhere?");
         System.out.println("Oh hey! I'm here, I'm in IntelliJ IDEA, Are we the last?");
+        System.out.println("What are those psychopaths saying, Tommy?");
     }
 }
